ba217ffb45c5c857a7e7d7cb01478b4d45aaee4f
Ala Abu Rish <alaa.rish2@gmail.com>
2021-08-21 21:17:46 +0300

    lab8 movies

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import Forecast from './Forecast.js';
 import dotenv from 'dotenv';
+import Movie from './Movie.js';
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ const server = express();
 const PORT = process.env.PORT;
 
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
+const MOVIE_API_KEY=process.env.MOVIE_API_KEY;
 
 const corsOpts = {
     origin: '*',
@@ -25,6 +28,31 @@ const corsOpts = {
     ],
   };
 server.use(cors(corsOpts));
+
+server.get('/movies', (request, response) => {
+  axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&language=en-US&query=${request.query.searchQuery}`).then(res => {
+    let length = 20;
+    if (length > res.data.results.length)
+      length = res.data.results.length;
+      console.log(res.data);
+    let arr = [];
+    for (let i = 0; i < length; i++) {
+      arr.push(new Movie(
+        res.data.results[i].title,
+        res.data.results[i].overview,
+        res.data.results[i].vote_average,
+        res.data.results[i].vote_count,
+        'https://image.tmdb.org/t/p/original/' + res.data.results[i].poster_path,
+        res.data.results[i].popularity,
+        res.data.results[i].release_date
+        ));
+    }
+    response.send(arr);
+  }).catch(err =>  {
+    response.status(500);  
+    response.send('Error: no movie found!');
+  })
+});
 
 server.get('/weather', (request, response) =>  {
     let lat = request.query.lat;
