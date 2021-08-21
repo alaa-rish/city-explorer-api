1bb5933ee1c0899ca658fef73bdf5639e880c98e
Ala Abu Rish <alaa.rish2@gmail.com>
2021-08-21 20:29:21 +0300

    lab8 weather

--- Forecast.js
+++ Forecast.js
@@ -7,4 +7,4 @@ class Forecast {
     }
 }
 
-module.exports = Forecast;
+export default Forecast;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-'use strict';
+import axios from 'axios';
+import express from 'express';
+import cors from 'cors';
+import Forecast from './Forecast.js';
+import dotenv from 'dotenv';
 
-const express = require('express');
+dotenv.config();
 
 const server = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 
-const cors = require("cors");
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
 const corsOpts = {
     origin: '*',
   
@@ -21,25 +26,26 @@ const corsOpts = {
   };
 server.use(cors(corsOpts));
 
-const weather = require("./data/weather");
-const Forecast = require('./Forecast');
-
 server.get('/weather', (request, response) =>  {
     let lat = request.query.lat;
     let lon = request.query.lon;
-    let searchQuery = request.query.searchQuery;
-    let res = weather.find(city => lat.startsWith(city.lat + '') && lon.startsWith(city.lon + '') && searchQuery.toLowerCase().includes(city.city_name.toLowerCase()));
-    if (res === undefined || res === null || res.length === 0) {
+    axios.get(`https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`).then(res => {
+      if (res === undefined || res === null || res.length === 0) {
         response.status(500);  
-      response.send('Error: no city found!');
+        response.send('Error: no city found!');
     } else {
         let data = [];
-        res.data.forEach(cityData => {
-            data.push(new Forecast(cityData.weather.description, cityData.valid_date));
+        console.log('>>>>> ' + `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`);
+        console.log(res.data);
+        res.data.data.forEach(cityData => {
+            data.push(new Forecast(cityData.weather.description, cityData.datetime));
         });
         response.send(data);
     }
-    
+    }).catch(err =>  {
+      response.status(500);  
+      response.send('Error: no city found!');
+    });
 });
 
 server.get('/test', (request, response) =>  {
